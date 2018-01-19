fd94c00fbd46425aa809d7a54dc4c9b495012946
Kiren <srinitude@gmail.com>
2018-01-19 01:04:00 +0000

    Adds module name into Flask app

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -4,7 +4,7 @@ dropdown menus and rental listings"""
 from flask import Flask, render_template
 from models import storage
 from uuid import uuid4
-app = Flask('web_flask')
+app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
